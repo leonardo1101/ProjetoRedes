db96803ecc9a00e69b04f72cc89141707a0d704a
Unknown <leonardo98ti@gmail.com>
2018-09-07 20:05:46 -0300

    Cliente quase 200 %

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -3,7 +3,7 @@
 import socket
 
 cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cli_socket.connect(('192.168.15.17', 2525))
+cli_socket.connect(('192.168.15.11', 2525))
 
 email_cli = input('Client\'s email: ')
 email_dest = input('Recipient\'s Email: ')
@@ -14,24 +14,25 @@ ip_server_dest = input('Recipient\'s server IP: ')
 sentence = 'HELO renata.ufscar'
 
 cli_socket.send(sentence.encode())
-
-
+response = cli_socket.recv(512).decode()
+print(response)
 if response[0:3] == '250':
 	sentence = 'MAIL FROM: <' + email_cli + '>'
 	cli_socket.send(sentence.encode())
-	response = cli_socket.recv(512)
+	response = cli_socket.recv(512).decode()
 	print(response)
 
 	if response[0:3] == '250':
+		print("entrei")
 		sentence = 'RCPT TO: <' + email_dest + '>'
 		cli_socket.send(sentence.encode())
-		response = cli_socket.recv(512)
+		response = cli_socket.recv(512).decode()
 		print(response)
 
 		if response[0:3] == '250':
 			sentence = 'DATA'
 			cli_socket.send(sentence.encode())
-			response = cli_socket.recv(512)
+			response = cli_socket.recv(512).decode()
 			print(response)
 
 			if response[0:3] == '354':
@@ -40,15 +41,14 @@ if response[0:3] == '250':
 				while data != '.':
 					sentence += data
 					data = input()
-
 				cli_socket.send(sentence.encode())
-				response = cli_socket.recv(512)
+				response = cli_socket.recv(512).decode()
 				print(response)
 
 				if response[0:3] == '250':
 					sentence = 'QUIT'
 					cli_socket.send(sentence.encode())
-					response = cli_socket.recv(512)
+					response = cli_socket.recv(512).decode()
 					print(response)
 
 cli_socket.close()
