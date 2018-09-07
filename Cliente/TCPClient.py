37cf35d262257ea4d8c7332bbdd9f9cb8771659b
Unknown <renatassmendes@hotmail.com>
2018-09-07 19:33:17 -0300

    Cliente

--- Cliente/TCPClient.py
+++ Cliente/TCPClient.py
@@ -1,11 +1,20 @@
-from socket import *
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+import socket
 
-nomeServidor = ''
-porta = 
-socketCliente = socket(AF_INET6,SOCK_STREAM)											#Cria o socket do cliente, 1º Param: IPv6 e 2º Param: SOCK_STREAM = TCP
-socketCliente.connect((nomeServidor,porta))												#Cria a conexão TCP, realizando 3 handshake para estabelecer a conexão
-sentenca = raw_input('Input lowercase sentence:')										#Pega a sentenca									
-socketCliente.send(sentenca.encode())													#Manda a sentenca atraves do socket para a conexão TCP
-setencaModificada = socketCliente.recv(1024)											#Espera o SO voltar para o processo, passando os pacotes do buffer, no maximo 2014
-print('From server '+nomeServidor+ ': '+ setencaModificada.decode())					#Decodifica a sentenca modificada e exibe na tela
-socketCliente.close()																	#Fecha a conexão TCP entre o cliente e servidor
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.15.17', 8901))
+
+sentence = 'helloooooooo todossssss'
+sentence += '#cambio'
+
+s.send(sentence.encode())
+
+modified = s.recv(1)
+while not b'#cambio' in modified:
+	modified += s.recv(1)
+
+modified = modified[:-7]
+print(modified.decode())
+
+s.close()
---
+++ Cliente/cliente.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+import socket
+
+cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cli_socket.connect(('192.168.15.17', 2525))
+
+email_cli = input('Client\'s email: ')
+email_dest = input('Recipient\'s Email: ')
+ip_server_cli = input('Client\'s server IP: ')
+ip_server_dest = input('Recipient\'s server IP: ')
+
+
+sentence = 'HELO renata.ufscar'
+
+cli_socket.send(sentence.encode())
+
+
+if response[0:3] == '250':
+	sentence = 'MAIL FROM: <' + email_cli + '>'
+	cli_socket.send(sentence.encode())
+	response = cli_socket.recv(512)
+	print(response)
+
+	if response[0:3] == '250':
+		sentence = 'RCPT TO: <' + email_dest + '>'
+		cli_socket.send(sentence.encode())
+		response = cli_socket.recv(512)
+		print(response)
+
+		if response[0:3] == '250':
+			sentence = 'DATA'
+			cli_socket.send(sentence.encode())
+			response = cli_socket.recv(512)
+			print(response)
+
+			if response[0:3] == '354':
+				sentence = ''
+				data = input()
+				while data != '.':
+					sentence += data
+					data = input()
+
+				cli_socket.send(sentence.encode())
+				response = cli_socket.recv(512)
+				print(response)
+
+				if response[0:3] == '250':
+					sentence = 'QUIT'
+					cli_socket.send(sentence.encode())
+					response = cli_socket.recv(512)
+					print(response)
+
+cli_socket.close()
